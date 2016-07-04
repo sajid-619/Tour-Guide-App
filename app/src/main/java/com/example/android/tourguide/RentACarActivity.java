d405e172b5550951d9fc65a7e6c2bd7a258b7f33
Sajid Zaman <sajid-619@users.noreply.github.com>
2016-07-04 23:21:59 +0600

    Create RentACarActivity.java

---
+++ app/src/main/java/com/example/android/tourguide/RentACarActivity.java
@@ -0,0 +1,33 @@
+package com.example.android.tourguideapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mpombos on 30/6/16.
+ */
+public class RentACarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.locations_list);
+
+        // Create a list of locations
+        final ArrayList<Locations> locations = new ArrayList<>();
+        locations.add(new Locations("Avis", R.string.avis_description));
+        locations.add(new Locations("Alamo", R.string.alamo_description));
+
+        //Create AdapterView adapter with this activity as the context, and the arrayList of Locations
+        LocationsAdapter adapter = new LocationsAdapter(this, locations);
+
+        //Create ListView listView
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        //set the AdapterView to the newly created listView
+        listView.setAdapter(adapter);
+    }
+}
