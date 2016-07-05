6896de9926c6901fef18383f75dfb5ed789eba74
Sajid Zaman <sajid-619@users.noreply.github.com>
2016-07-05 15:23:25 +0600

    Create LocationsAdapter.java

---
+++ app/src/main/java/com/example/android/tourguide/LocationsAdapter.java
@@ -0,0 +1,71 @@
+package com.example.android.tourguideapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mpombos on 30/6/16.
+ */
+public class LocationsAdapter extends ArrayAdapter<Locations> {
+
+    /**
+     * Create a new {@link LocationsAdapter} object.
+     *
+     * @param context is the current context (i.e. Activity) that the adapter is being created in.
+     * @param locations is the list of {@link Locations}s to be displayed.
+     */
+    public LocationsAdapter(Context context, ArrayList<Locations> locations) {
+        super(context, 0, locations);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if an existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link Word} object located at this position in the list
+        Locations currentLocation = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID name_text_view.
+        TextView nameTextView = (TextView) listItemView.findViewById(R.id.name_text_view);
+
+        // Get the Name from the currentLocation object and set this text on
+        // the Name TextView.
+        nameTextView.setText(currentLocation.getName());
+
+        // Find the TextView in the list_item.xml layout with the ID description_text_view.
+        TextView descriptionTextView = (TextView) listItemView.findViewById(R.id.description_text_view);
+
+        // Get the Description from the currentWord object and set this text on
+        // the default TextView.
+        descriptionTextView.setText(currentLocation.getDescription());
+
+        // Find the ImageView in the list_item.xml layout with the ID image.
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+
+        if (currentLocation.hasImage()) {
+            // If an image is available, display the provided image based on the resource ID
+            imageView.setImageResource(currentLocation.getImageResourceId());
+            // Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
+
+        // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
+        // the ListView.
+        return listItemView;
+    }
+}
